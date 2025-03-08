4eb2919842933dd781ecf45b7e8733c81c0d2768
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-08 10:56:23 +0200

    0.01

---
+++ contracts/AllodiumEtherV1.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Allodium {
+  string public name = "Allodium";
+  string public symbol = "ALOD";
+  string public standart = "@Allodium v.0.1";
+  string public totalSupply;
+  string public ownerOfContract;
+  string public _userId;
+
+  address[] public holderToken;
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
+  mapping(address => TokenHolderInfo) public tokenHolderInfos;
+
+  struct TokenHolderInfo{
+    uint256 _tokenId;
+    address _from;
+    address _to;
+    uint256 _total;
+    bool _tokenHolder;
+  }
+
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
+
+  constructor(uint256 _initialSupply) {
+    ownerOfContract = msg.sender;
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+  }
+
+  function inc() internal {
+    _userId++;
+  }
+
+  function transfer(address _to, uint256 _value) public returns(bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    inc();
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    TokenHolderInfo storage tokenHolderInfo = tokenHolderInfos[_to];
+
+    tokenHolderInfo._to = _to;
+    tokenHolderInfo._from = msg.sender;
+    tokenHolderInfo._tokenHolder = true;
+    tokenHolderInfo._totalToken = _value;
+    tokenHolderInfo._tokenId = _userId;
+
+    holderToken.push(_to);
+
+    emit Transfer(msg.sender, _to, _value);
+    
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    allowance[_from][msg.sender] -= _value;
+
+    emit Transfer(_from, _to, _value);
+
+    return true;
+  }
+
+  function getTokenHolderData(address _address) public view returns(
+    
+  )
+
+}
