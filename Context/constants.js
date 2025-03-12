d165ae45e7660fe40855c7587305b41c09a884c0
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-12 06:22:54 +0200

    0.07

---
+++ Context/constants.js
@@ -0,0 +1,12 @@
+
+// Allodium = 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+// TokenSale = 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
+
+import Allodium from './Allodium.json';
+import TokenSale from './TokenSale.json';
+
+export const TOKEN_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+export const TOKEN_ABI = Allodium.abi;
+
+export const TOKEN_SALE_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+export const TOKEN_SALE_ABI = TokenSale.abi;
